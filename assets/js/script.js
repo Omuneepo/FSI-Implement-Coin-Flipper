5e3f176556d266a6ef40dedceabce1149b29f55d
Andrei Alcocer <andreialcocer@Andreis-MacBook-Air.local>
2022-12-01 02:15:06 -0800

    i finished it

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,66 @@
 // TODO: Declare any global variables we need
+let headsFlip = 0;
+let tailsFlip = 0;
 
+function updateScoreboard() {
+    let totalFlipCount = headsFlip + tailsFlip; 
+    let percentHeads = 0;
+    let percentTails = 0;
+    if (totalFlipCount > 0) {
+      percentHeads = Math.round((headsFlip / totalFlipCount) * 100);
+      percentTails = Math.round((tailsFlip / totalFlipCount) * 100);
+
+   }
+    
+    document.querySelector("#heads").textContent = headsFlip;
+    document.querySelector("#heads-percent").textContent = percentHeads + "%";
+    document.querySelector("#tails").textContent = tailsFlip;
+    document.querySelector("#tails-percent").textContent = percentTails + "%";
+}  
 
 document.addEventListener('DOMContentLoaded', function () {
-
-    document.addEventListener('DOMContentLoaded', function () {
-        let flipButton = document.querySelector('#flip');
+    let flipButton = document.querySelector('#flip');
         flipButton.addEventListener('click', function() {
-            let randomNumber = math.random();
-        })
-    }
-    )
-    // This is just a sanity check to make sure your JavaScript script is getting loaded
-    // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
-
-    // TODO: Add event listener and handler for flip and clear buttons
-
-    // Flip Button Click Handler
-        // TODO: Determine flip outcome
-        let isHeads = true
-        if (randomNumber < 0.50){
-            isHeads = False;
-        }
-        // TODO: Update image and status message in the DOM
+            let randomNumber = Math.random();
+            let isHeads = true;
+            if (randomNumber >0.5) {
+                isHeads= false;
+             }
         let coin = document.querySelector('#coin');
         let status = document.querySelector('#status');
 
         if (isHeads) {
-            coin.src = "assets/ images/penny-heads.jpg";
+            coin.src = "assets/images/penny-heads.jpg";
             coin.alt = "penny (heads side)";
-            status.textContext = "you flipped a heads!";
-            headsFlipcount++;
-        } else {
-            coin.src = "assets/images/penny-tail.jpg"
+            status.textContent = "you flipped heads!";
+            headsFlip++;
+            } else {
+            coin.src = "assets/images/penny-tails.jpg"
             coin.alt = "penny (tails side)"
-            status.textContent = "you flipped a tails!"
-            tailFlipcount++;
-        }
-
-        // Update the scorboard 
-        // TODO: Calculate the total number of rolls/flips
-        let totalFlip = headsFlipcount + tailFlipcount; 
-
-        // Make variables to track the percentages of heads and tails
-        // TODO: Use the calculated total to calculate the percentages
-     
-        let precentHeads = headsFlipcount / totalFlipcount;
-        let precentTails = tailFlipcount / totalFlipcount;
-
-
-        // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
-        // TODO: Update the display of each table cell
-        document.querySelector("#heads").textContent = headsFlipcount;
-        document.querySelector("#heads-precent").textContent = precentHeads
-        document.querySelector("#tails").textContent = tailFlipcount
-        document.querySelector("#tails-precent").textContent = precentTails
-
-
-    // Clear Button Click Handler
+            status.textContent= "you flipped tails!";
+            tailsFlip++;
+            }
+            updateScoreboard();
+            
+            
+    });
     let clearButton = document.querySelector("#clear");
+    clearButton.addEventListener('click', function() {
+        headsFlip = 0;
+        tailsFlip = 0;
 
-        // TODO: Reset global variables to 0
-        // TODO: Update the scoreboard (same logic as in flip button click handler)
+        let coin = document.querySelector('#coin');
+        let status = document.querySelector('#status');
 
-})
+        coin.src = "assets/images/penny-heads.jpg";
+        coin.alt = "penny (heads side)";
+        status.textContent = "lets get flipped!";
+
+        updateScoreboard();
+
+    });
+
+
+})    
+    
+
